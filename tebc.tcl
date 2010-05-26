6ea9de596519cee3ce2016c8c31548255a334ec1
Jos Decoster <jos.decoster@gmail.com>
2010-05-26 14:51:23 +0200

    iir example

--- tebc.tcl
+++ tebc.tcl
@@ -4,7 +4,9 @@ package require llvmtcl
 namespace import llvmtcl::*
 
 set optimize 1
-set procs {test2 test test3 test4 test5 fact facti fact10}
+set procs {test2 test test3 test4 test5 fact facti fact10 low_pass filter}
+set timings {low_pass filter}
+set timing_count 10
 
 proc test {a b c d e} {
     if {$a <= 66 && $a > 50} {
@@ -16,7 +18,7 @@ proc test {a b c d e} {
 }
 
 proc test2 {a b c d e} {
-    return [expr {222*($a+444)}]
+    return [expr {4+$a+6}]
 }
 
 proc test3 {a b c d e} {
@@ -50,6 +52,32 @@ proc fact10 { } {
     return [fact 10]
 }
 
+proc low_pass {x x1 x2 y1 y2 C0 C1 C2 C3 C4} {
+    return [expr {$x*$C0 + $x1*$C1 + $x2*$C2 + $y1*$C3 + $y2*$C4}]
+}
+
+proc filter { } {
+    set y 0
+    set x1 0
+    set x2 0
+    set y1 0
+    set y2 0
+    for {set i 0} {$i < 1000} {incr i} {
+	set y [low_pass $i $x1 $x2 $y1 $y2 1 3 -2 4 -5]
+	# Messing with the result to stay within 32 bit
+	if {$y > 1000 || $y < -1000} {
+	    set y 1
+	} else {
+	    set y1 $y
+	}
+	set y2 $y1
+	set y1 $y
+	set x2 $x1
+	set x1 [expr {$i}]
+    }
+    return $y
+}
+
 # Initialize the JIT
 LLVMLinkInJIT
 LLVMInitializeNativeTarget
@@ -62,6 +90,7 @@ foreach nm $procs {
     set func($nm) [Tcl2LLVM $m $nm 1]
 }
 foreach nm $procs {
+    puts "$nm ######################################################################"
     set func($nm) [Tcl2LLVM $m $nm]
 }
 
@@ -78,20 +107,21 @@ if {$rt} {
 
 # Optimize functions and module
 if {$optimize} {
-    set td [LLVMCreateTargetData ""]
-    LLVMSetDataLayout $m [LLVMCopyStringRepOfTargetData $td]
-    foreach {nm f} [array get func] {
-	LLVMOptimizeFunction $m $f 3 $td
+    for {set i 0} {$i < 10} {incr i} {
+	set td [LLVMCreateTargetData ""]
+	LLVMSetDataLayout $m [LLVMCopyStringRepOfTargetData $td]
+	foreach {nm f} [array get func] {
+	    LLVMOptimizeFunction $m $f 3 $td
+	}
+	LLVMOptimizeModule $m 3 0 1 1 1 0 $td
     }
-    LLVMOptimizeModule $m 3 0 1 1 1 0 $td
 }
 
+set f [open tebc-optimized.ll w]
+puts $f [LLVMDumpModule $m]
+close $f
+
 # Some tests
-set tclArgs {5 2 3 4 5}
-set llvmArgs {}
-foreach v $tclArgs {
-    lappend llvmArgs [LLVMCreateGenericValueOfInt [LLVMInt32Type] $v 0]
-}
 
 lassign [LLVMCreateJITCompilerForModule $m 0] rt EE msg
 
@@ -99,29 +129,39 @@ puts "OK? Tcl        LLVM       Function"
 puts "--- ---------- ---------- ------------------------------------"
 foreach nm $procs {
     switch -glob -- $nm {
-	"test*" {
-	    set la $llvmArgs
-	    set ta $tclArgs
-	}
+	"filter*" -
 	"fact10" {
-	    set la {}
-	    set ta {}
+	    set la($nm) {}
+	    set ta($nm) {}
 	}
 	"fact*" {
-	    set la [lindex $llvmArgs 0]
-	    set ta [lindex $tclArgs 0]
+	    set ta($nm) 5
+	    set la($nm) [LLVMCreateGenericValueOfInt [LLVMInt32Type] 5 0]
+	}
+	"low_pass*" {
+	    set ta($nm) {500 1000 2000 1234 5678 1341 2682 1341 16607 -5591}
+	    set la($nm) {}
+	    foreach v $ta($nm) {
+		lappend la($nm) [LLVMCreateGenericValueOfInt [LLVMInt32Type] $v 0]
+	    }
+	}
+	default {
+	    set ta($nm) {5 2 3 4 5}
+	    set la($nm) {}
+	    foreach v $ta($nm) {
+		lappend la($nm) [LLVMCreateGenericValueOfInt [LLVMInt32Type] $v 0]
+	    }
 	}
     }
-    set res [LLVMRunFunction $EE $func($nm) $la]
-    set tr [$nm {*}$ta]
+    set res [LLVMRunFunction $EE $func($nm) $la($nm)]
+    set tr [$nm {*}$ta($nm)]
     set lr [expr {int([LLVMGenericValueToInt $res 0])}]
     puts "[expr {$tr==$lr?"OK ":"ERR"}] [format %10d $tr] [format %10d $lr] $nm"
 }
 
-set v [LLVMCreateGenericValueOfInt [LLVMInt32Type] 10 0]
-puts ""
-puts "tcl \[fact 10\]  : [time {fact 10} 10000]"
-puts "tcl \[fact10\]   : [time {fact10} 10000]"
-puts "llvm \[fact 10\] : [time {LLVMRunFunction $EE $func(fact) $v} 10000]"
-puts "llvm \[fact10\]  : [time {LLVMRunFunction $EE $func(fact10) {}} 10000]"
+foreach nm $timings {
+    puts "tcl \[$nm\]: [time {$nm {*}$ta($nm)} $timing_count]"
+    puts "llvm \[$nm\]: [time {LLVMRunFunction $EE $func($nm) $la($nm)} $timing_count]"
+}
+
 

7fa2d5c4d10620f6e98f87467f54e2e86b4316e0
Donal Fellows <donal.k.fellows@manchester.ac.uk>
2015-12-24 20:22:24 +0000

    Added forgotten header file.

---
+++ generic/llvmtcl.h
@@ -0,0 +1,49 @@
+#include "tcl.h"
+#include <string>
+#include "llvm/IR/Module.h"
+#include "llvm/IR/Type.h"
+#include "llvm/IR/Value.h"
+#include "llvm/ExecutionEngine/ExecutionEngine.h"
+
+MODULE_SCOPE int	GetEngineFromObj(Tcl_Interp *interp, Tcl_Obj *obj,
+			    llvm::ExecutionEngine *&engine);
+MODULE_SCOPE int	GetModuleFromObj(Tcl_Interp *interp, Tcl_Obj *obj,
+			    llvm::Module *&module);
+MODULE_SCOPE std::string GetRefName(std::string prefix);
+MODULE_SCOPE int	GetTypeFromObj(Tcl_Interp *interp, Tcl_Obj *obj,
+			    llvm::Type *&type);
+template<typename T>
+MODULE_SCOPE int	GetTypeFromObj(Tcl_Interp *interp, Tcl_Obj *obj,
+			    std::string msg, T *&type);
+MODULE_SCOPE int	GetValueFromObj(Tcl_Interp *interp, Tcl_Obj *obj,
+			    llvm::Value *&module);
+template<typename T>
+MODULE_SCOPE int	GetValueFromObj(Tcl_Interp *interp, Tcl_Obj *obj,
+			    std::string msg, T *&value);
+
+#define DECL_CMD(cName) \
+    MODULE_SCOPE int cName(ClientData clientData, Tcl_Interp *interp, \
+	    int objc, Tcl_Obj *const objv[]);
+
+DECL_CMD(CreateDebugBuilder);
+DECL_CMD(DisposeDebugBuilder);
+DECL_CMD(DefineCompileUnit);
+DECL_CMD(DefineFile);
+DECL_CMD(DefineNamespace);
+DECL_CMD(DefineUnspecifiedType);
+DECL_CMD(DefineAliasType);
+DECL_CMD(DefineBasicType);
+DECL_CMD(DefinePointerType);
+DECL_CMD(DefineStructType);
+DECL_CMD(DefineFunctionType);
+DECL_CMD(DefineFunction);
+DECL_CMD(AttachToFunction);
+DECL_CMD(LLVMAddLLVMTclCommandsObjCmd);
+
+/*
+ * Local Variables:
+ * mode: c++
+ * c-basic-offset: 4
+ * fill-column: 78
+ * End:
+ */
